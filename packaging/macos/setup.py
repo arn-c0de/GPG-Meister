6de46ba47428e65965c4e4a147f67cb7666ae322
arn-c0de <arn-c0de@protonmail.com>
2026-05-15 17:27:05 +0200

    Bump version to 1.0.3

--- packaging/macos/setup.py
+++ packaging/macos/setup.py
@@ -20,8 +20,8 @@ OPTIONS = {
         "CFBundleDisplayName": "GPG Meister",
         "CFBundleName": "GPG Meister",
         "CFBundleIdentifier": "io.github.arn-c0de.GPGMeister",
-        "CFBundleShortVersionString": "1.0.2",
-        "CFBundleVersion": "1.0.2",
+        "CFBundleShortVersionString": "1.0.3",
+        "CFBundleVersion": "1.0.3",
     },
 }
 
